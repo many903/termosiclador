f2bc39c7a46a8d5835cd678412430bbc967535bd
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2023-02-03 00:33:48 -0600

    creacion de botones y de funciones sin terminar

--- main.py
+++ main.py
@@ -14,9 +14,33 @@ ventana.geometry("500x500") #tamaño de la venatana
 #ventana.iconbitmap("form.ico") #Cambiar el icono no hasta escojer un icono 
 ventana.config(bg="gray")#color de la ventana
 
-#ajuste y personalizacion de la ventana
-boton = tk.Button(text="Boton")
-boton.place(x=50, y=50)
+#ajuste y personalizacion de la ventana del sistema 
+#boton de play
+boton = tk.Button(text="play")
+boton.place(x=450, y=50)
+
+#boton de pausa
+botonII = tk.Button(text="puase")
+botonII.place(x=400, y=50)
 
 ventana.mainloop()#se mantiene la vantana este simpre tiene que estar al final de la config. de la ventana
 
+#funciones del programa
+#funcion del boton play
+#def play():
+    
+
+
+#funcion del boton pause
+#def pausa():
+
+
+
+
+#main loop
+preca = int(input(" la temperatura de precalentado :"))
+time1 =
+time2 =
+time3 =
+time4 =
+enfriamiento =
