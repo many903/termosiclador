2cb5bc7b114580e857fa528bb4daa4f93d24b9f6
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2023-04-03 22:03:12 -0600

    1.1.1.11 pruevas

--- prueba.py
+++ prueba.py
@@ -1,123 +1,105 @@
-from tkinter import *
-from tkinter import messagebox
- 
- 
-class Pycalc(Frame):
- 
-    def __init__(self, master, *args, **kwargs):
-        Frame.__init__(self, master, *args, **kwargs)
-        self.parent = master
-        self.grid()
-        self.createWidgets()
- 
-    def deleteLastCharacter(self):
-        textLength = len(self.display.get())
- 
-        if textLength >= 1:
-            self.display.delete(textLength - 1, END)
-        if textLength == 1:
-            self.replaceText("0")
- 
-    def replaceText(self, text):
-        self.display.delete(0, END)
-        self.display.insert(0, text)
- 
-    def append(self, text):
-        actualText = self.display.get()
-        textLength = len(actualText)
-        if actualText == "0":
-            self.replaceText(text)
-        else:
-            self.display.insert(textLength, text)
- 
-    def evaluate(self):
-        try:
-            self.replaceText(eval(self.display.get()))
-        except (SyntaxError, AttributeError):
-            messagebox.showerror("Error", "Syntax Error")
-            self.replaceText("0")
-        except ZeroDivisionError:
-            messagebox.showerror("Error", "Cannot Divide by 0")
-            self.replaceText("0")
- 
-    def containsSigns(self):
-        operatorList = ["*", "/", "+", "-"]
-        display = self.display.get()
-        for c in display:
-            if c in operatorList:
-                 return True
-        return False
- 
-    def changeSign(self):
-        if self.containsSigns():
-            self.evaluate()
-        firstChar = self.display.get()[0]
-        if firstChar == "0":
-            pass
-        elif firstChar == "-":
-            self.display.delete(0)
-        else:
-            self.display.insert(0, "-")
- 
-    def inverse(self):
-        self.display.insert(0, "1/(")
-        self.append(")")
-        self.evaluate()
- 
-    def createWidgets(self):
-        self.display = Entry(self, font=("Arial", 24), relief=RAISED, justify=RIGHT, bg='darkblue', fg='red', borderwidth=0)
-        self.display.insert(0, "0")
-        self.display.grid(row=0, column=0, columnspan=4, sticky="nsew")
- 
-        self.ceButton = Button(self, font=("Arial", 12), fg='red', text="CE", highlightbackground='red', command=lambda: self.replaceText("0"))
-        self.ceButton.grid(row=1, column=0, sticky="nsew")
-        self.inverseButton = Button(self, font=("Arial", 12), fg='red', text="1/x", highlightbackground='lightgrey', command=lambda: self.inverse())
-        self.inverseButton.grid(row=1, column=2, sticky="nsew")
-        self.delButton = Button(self, font=("Arial", 12), fg='#e8e8e8', text="Del", highlightbackground='red', command=lambda: self.deleteLastCharacter())
-        self.delButton.grid(row=1, column=1, sticky="nsew")
-        self.divButton = Button(self, font=("Arial", 12), fg='red', text="/", highlightbackground='lightgrey', command=lambda: self.append("/"))
-        self.divButton.grid(row=1, column=3, sticky="nsew")
- 
-        self.sevenButton = Button(self, font=("Arial", 12), fg='white', text="7", highlightbackground='black', command=lambda: self.append("7"))
-        self.sevenButton.grid(row=2, column=0, sticky="nsew")
-        self.eightButton = Button(self, font=("Arial", 12), fg='white', text="8", highlightbackground='black', command=lambda: self.append("8"))
-        self.eightButton.grid(row=2, column=1, sticky="nsew")
-        self.nineButton = Button(self, font=("Arial", 12), fg='white', text="9", highlightbackground='black', command=lambda: self.append("9"))
-        self.nineButton.grid(row=2, column=2, sticky="nsew")
-        self.multButton = Button(self, font=("Arial", 12), fg='red', text="*", highlightbackground='lightgrey', command=lambda: self.append("*"))
-        self.multButton.grid(row=2, column=3, sticky="nsew")
- 
-        self.fourButton = Button(self, font=("Arial", 12), fg='white', text="4", highlightbackground='black', command=lambda: self.append("4"))
-        self.fourButton.grid(row=3, column=0, sticky="nsew")
-        self.fiveButton = Button(self, font=("Arial", 12), fg='white', text="5", highlightbackground='black', command=lambda: self.append("5"))
-        self.fiveButton.grid(row=3, column=1, sticky="nsew")
-        self.sixButton = Button(self, font=("Arial", 12), fg='white', text="6", highlightbackground='black', command=lambda: self.append("6"))
-        self.sixButton.grid(row=3, column=2, sticky="nsew")
-        self.minusButton = Button(self, font=("Arial", 12), fg='red', text="-", highlightbackground='lightgrey', command=lambda: self.append("-"))
-        self.minusButton.grid(row=3, column=3, sticky="nsew")
- 
-        self.oneButton = Button(self, font=("Arial", 12), fg='white', text="1", highlightbackground='black', command=lambda: self.append("1"))
-        self.oneButton.grid(row=4, column=0, sticky="nsew")
-        self.twoButton = Button(self, font=("Arial", 12), fg='white', text="2", highlightbackground='black', command=lambda: self.append("2"))
-        self.twoButton.grid(row=4, column=1, sticky="nsew")
-        self.threeButton = Button(self, font=("Arial", 12), fg='white', text="3", highlightbackground='black', command=lambda: self.append("3"))
-        self.threeButton.grid(row=4, column=2, sticky="nsew")
-        self.plusButton = Button(self, font=("Arial", 12), fg='red', text="+", highlightbackground='lightgrey', command=lambda: self.append("+"))
-        self.plusButton.grid(row=4, column=3, sticky="nsew")
- 
-        self.negToggleButton = Button(self, font=("Arial", 12), fg='red', text="+/-", highlightbackground='lightgrey', command=lambda: self.changeSign())
-        self.negToggleButton.grid(row=5, column=0, sticky="nsew")
-        self.zeroButton = Button(self, font=("Arial", 12), fg='white', text="0", highlightbackground='black', command=lambda: self.append("0"))
-        self.zeroButton.grid(row=5, column=1, sticky="nsew")
-        self.decimalButton = Button(self, font=("Arial", 12), fg='white', text=".", highlightbackground='lightgrey', command=lambda: self.append("."))
-        self.decimalButton.grid(row=5, column=2, sticky="nsew")
-        self.equalsButton = Button(self, font=("Arial", 12), fg='red', text="=", highlightbackground='lightgrey', command=lambda: self.evaluate())
-        self.equalsButton.grid(row=5, column=3, sticky="nsew")
- 
- 
-Calculator = Tk()
-Calculator.title("AdictoCalculator")
-Calculator.resizable(False, False)
-Calculator.config(cursor="pencil")
-root = Pycalc(Calculator).grid()
-Calculator.mainloop()
+
+import serial,time,collections
+import matplotlib.pyplot as plt
+import matplotlib.animation as animacion
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from threading import Thread
+from tkinter import Tk, Frame, StringVar, Label,Button,Entry
+
+isReceiving= False 
+isRun = True 
+datos = 0.0
+muestraD = 100
+data = collections.deque([0]*muestraD, maxlen=muestraD)
+xmin = 0
+xmax = muestraD
+ymin = -5
+ymax = 5 
+#"/dev/ttyUSBB"
+try:
+    arduino = serial.Serial("COM6", 9600 , timeout=1)          
+except:
+    print("Error de coneccion con el puerto")
+
+def Iniciar():
+    global datos
+    global isReceiving
+    global isRun
+    isReceiving = True
+    isRun = True   
+    thread.start() 
+    anim = animacion.FuncAnimation(fig, plotData,  fargs=(muestraD,lines),interval = 100, blit = False )
+    plt.show()
+    
+def DatosA():
+    time.sleep(1)
+    arduino.reset_input_buffer()
+    while (isRun):
+        global isReceive
+        global datos 
+        datos = float(arduino.readline().decode('utf-8'))
+        isReceive = True
+        
+def plotData(self,muestraD,lines):
+    data.append(datos)
+    lines.set_data(range(muestraD), data)
+    labelx.set("VOL:" + str(datos)) 
+thread = Thread(target = DatosA) 
+
+def Salir():
+    global isRun
+    isRun = False 
+    thread.join()
+    arduino.close()
+    time.sleep(1)
+    raiz.destroy()
+    raiz.quit()
+    print("proceso finalizado")
+def Terminar():  
+    global isRun
+    global isReceiving 
+    isRun = False 
+    isReceiving = False
+    time.sleep(0.5)
+    thread.join(timeout=0.3)
+    arduino.close()
+    datos=00.0
+    
+# , figsize=(6, 5)   tamaño / , dpi=75  zoom / plt.cla()  borra  nombres x e y /
+fig = plt.figure(facecolor="0.55",figsize=(6, 4), clear=True, dpi=100)
+ax = plt.axes(xlim=(xmin,xmax),ylim=(ymin,ymax))
+plt.title("Grafica - 0 - 5 Voltios",color='red',size=16, family="Tahoma")
+ax.set_xlabel("Muestras")
+ax.set_ylabel("Señal")
+lines = ax.plot([] ,[], 'r')[0]
+
+def Limpiar():
+    fig.clf()
+    
+raiz = Tk()
+raiz.protocol("WM_DELATE_WINDOW",Salir)
+raiz.config(bg = "black")
+raiz.title("  \t\t\t\t GRAFICA SEÑAL ANALOGICA")
+raiz.geometry("738x402")
+raiz.resizable(1,1)
+
+lienzo = FigureCanvasTkAgg(fig, master = raiz )
+lienzo._tkcanvas.grid(row = 0,column = 0, padx = 1,pady = 1)
+frame = Frame(raiz, width = 130,height = 402, bg = "#7003FC")
+frame.grid(row = 0,column = 1, padx = 1,pady = 2)
+frame.grid_propagate(False)
+frame.config(relief = "sunken")
+frame.config(cursor = "heart")
+labelx = StringVar(raiz, "VOL: 0.00")
+
+label = Label(frame,textvariable = labelx, bg= "#5CFE05",fg="black", font="Helvetica 13 bold",width=11 ,justify="center")
+label.grid(row=0,column=0, padx=5,ipady=8, pady=10)
+Iniciar = Button(frame,command= Iniciar, text= "Iniciar ",bg="blue",fg="white", font="Helvetica 14 bold",width=9,justify="center")
+Iniciar.grid(row=1,column=0, padx=5,pady=5)
+terminar = Button(frame,command= Terminar, text= "Terminar",bg="blue",fg="white", font="Helvetica 14 bold",width=9)
+terminar.grid(row=2,column=0, padx=5,pady=5)
+limpiar = Button(frame,command= Limpiar, text= "Limpiar ",bg="blue",fg="white", font="Helvetica 14 bold",width=9,justify="center")
+limpiar.grid(row=3,column=0, padx=5,pady=5)
+salir = Button(frame,command= Salir, width=9 ,text= "SALIR",bg="red", font="Helvetica 14 bold",justify="center")
+salir.grid(row=4,column=0, padx=5,pady=125)
+raiz.mainloop()
